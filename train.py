9aedf738629a743118791d156939b1463b3cb22c
tongxingyu <tongxingyu@ln.chinamobile.com>
2021-08-03 19:55:52 +0800

    Init source.

---
+++ train.py
@@ -0,0 +1,109 @@
+import os
+import time
+
+import paddle
+import paddle.nn.functional as F
+from dataset import Dataset
+from hed_model import HED
+from transforms import Normalize
+from timer import TimeAverager, calculate_eta
+import logger
+
+save_dir = 'output'
+iters = 100000
+save_interval = 1000
+batch_size = 1
+transforms = [
+    Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+]
+dataset = Dataset(transforms=transforms,
+                  dataset_root="/Users/alex/baidu/HED-BSDS",
+                  train_path="/Users/alex/baidu/HED-BSDS/train_pair.lst")
+
+batch_sampler = paddle.io.DistributedBatchSampler(
+    dataset, batch_size=batch_size, shuffle=True, drop_last=True)
+
+loader = paddle.io.DataLoader(
+    dataset,
+    batch_sampler=batch_sampler,
+    num_workers=0,
+    return_list=True,
+)
+iter = 0
+iters_per_epoch = len(batch_sampler)
+log_iters = 1
+avg_loss = 0.0
+avg_loss_list = []
+reader_cost_averager = TimeAverager()
+batch_cost_averager = TimeAverager()
+model = HED(pretrained='/Users/alex/Downloads/VGG16_pretrained.pdparams')
+learning_rate = paddle.optimizer.lr.StepDecay(learning_rate=1e-3, step_size=10000, gamma=0.1)
+optimizer = paddle.optimizer.Momentum(learning_rate=learning_rate, parameters=model.parameters(), weight_decay=2e-4)
+
+batch_start = time.time()
+while iter < iters:
+    for data in loader:
+        iter += 1
+        if iter > iters:
+            break
+        images = data[0]
+        labels = data[1]
+        logits_list = model(images)
+        loss_list = []
+        for logits in logits_list:
+            loss = F.binary_cross_entropy_with_logits(logits, labels)
+            loss_list.append(loss)
+        loss = sum(loss_list)
+        loss.backward()
+        optimizer.step()
+        model.clear_gradients()
+
+        lr = optimizer.get_lr()
+
+        # update lr
+        if isinstance(optimizer, paddle.distributed.fleet.Fleet):
+            lr_sche = optimizer.user_defined_optimizer._learning_rate
+        else:
+            lr_sche = optimizer._learning_rate
+        if isinstance(lr_sche, paddle.optimizer.lr.LRScheduler):
+            lr_sche.step()
+
+        avg_loss += loss.numpy()[0]
+        if not avg_loss_list:
+            avg_loss_list = [l.numpy() for l in loss_list]
+        else:
+            for i in range(len(loss_list)):
+                avg_loss_list[i] += loss_list[i].numpy()
+        batch_cost_averager.record(
+            time.time() - batch_start, num_samples=batch_size)
+
+        if (iter) % log_iters == 0:
+            avg_loss /= log_iters
+            avg_loss_list = [l[0] / log_iters for l in avg_loss_list]
+            remain_iters = iters - iter
+            avg_train_batch_cost = batch_cost_averager.get_average()
+            avg_train_reader_cost = reader_cost_averager.get_average()
+            eta = calculate_eta(remain_iters, avg_train_batch_cost)
+            logger.info(
+                "[TRAIN] epoch: {}, iter: {}/{}, loss: {:.4f}, lr: {:.6f}, batch_cost: {:.4f}, reader_cost: {:.5f}, ips: {:.4f} samples/sec | ETA {}"
+                    .format((iter - 1) // iters_per_epoch + 1, iter, iters,
+                            avg_loss, lr, avg_train_batch_cost,
+                            avg_train_reader_cost,
+                            batch_cost_averager.get_ips_average(), eta))
+
+            avg_loss = 0.0
+            avg_loss_list = []
+            reader_cost_averager.reset()
+            batch_cost_averager.reset()
+
+        if (iter % save_interval == 0 or iter == iters):
+            current_save_dir = os.path.join(save_dir,
+                                            "iter_{}".format(iter))
+            if not os.path.isdir(current_save_dir):
+                os.makedirs(current_save_dir)
+            paddle.save(model.state_dict(),
+                        os.path.join(current_save_dir, 'model.pdparams'))
+            paddle.save(optimizer.state_dict(),
+                        os.path.join(current_save_dir, 'model.pdopt'))
+        batch_start = time.time()
+
---
+++ vgg.py
@@ -0,0 +1,137 @@
+import paddle
+from paddle import ParamAttr
+import paddle.nn as nn
+import paddle.nn.functional as F
+from paddle.nn import Conv2D, BatchNorm, Linear, Dropout
+from paddle.nn import AdaptiveAvgPool2D, MaxPool2D, AvgPool2D
+from utils import load_pretrained_model
+
+__all__ = ["VGG11", "VGG13", "VGG16", "VGG19"]
+
+
+class ConvBlock(nn.Layer):
+    def __init__(self, input_channels, output_channels, groups, name=None):
+        super(ConvBlock, self).__init__()
+
+        self.groups = groups
+        self._conv_1 = Conv2D(
+            in_channels=input_channels,
+            out_channels=output_channels,
+            kernel_size=3,
+            stride=1,
+            padding=35 if 'conv1' in name else 1,
+            weight_attr=ParamAttr(name=name + "1_weights"),
+            bias_attr=False)
+        if groups == 2 or groups == 3 or groups == 4:
+            self._conv_2 = Conv2D(
+                in_channels=output_channels,
+                out_channels=output_channels,
+                kernel_size=3,
+                stride=1,
+                padding=1,
+                weight_attr=ParamAttr(name=name + "2_weights"),
+                bias_attr=False)
+        if groups == 3 or groups == 4:
+            self._conv_3 = Conv2D(
+                in_channels=output_channels,
+                out_channels=output_channels,
+                kernel_size=3,
+                stride=1,
+                padding=1,
+                weight_attr=ParamAttr(name=name + "3_weights"),
+                bias_attr=False)
+        if groups == 4:
+            self._conv_4 = Conv2D(
+                in_channels=output_channels,
+                out_channels=output_channels,
+                kernel_size=3,
+                stride=1,
+                padding=1,
+                weight_attr=ParamAttr(name=name + "4_weights"),
+                bias_attr=False)
+
+        self._pool = MaxPool2D(kernel_size=2, stride=2, padding=0)
+
+    def forward(self, inputs):
+        x = self._conv_1(inputs)
+        x = F.relu(x)
+        if self.groups == 2 or self.groups == 3 or self.groups == 4:
+            x = self._conv_2(x)
+            x = F.relu(x)
+        if self.groups == 3 or self.groups == 4:
+            x = self._conv_3(x)
+            x = F.relu(x)
+        if self.groups == 4:
+            x = self._conv_4(x)
+            x = F.relu(x)
+        pool = self._pool(x)
+        return pool, x
+
+
+class VGGNet(nn.Layer):
+    def __init__(self, layers=11, stop_grad_layers=0, pretrained=None):
+        super(VGGNet, self).__init__()
+
+        self.layers = layers
+        self.stop_grad_layers = stop_grad_layers
+        self.vgg_configure = {
+            11: [1, 1, 2, 2, 2],
+            13: [2, 2, 2, 2, 2],
+            16: [2, 2, 3, 3, 3],
+            19: [2, 2, 4, 4, 4]
+        }
+        assert self.layers in self.vgg_configure.keys(), \
+            "supported layers are {} but input layer is {}".format(
+                self.vgg_configure.keys(), layers)
+        self.groups = self.vgg_configure[self.layers]
+
+        self._conv_block_1 = ConvBlock(3, 64, self.groups[0], name="conv1_")
+        self._conv_block_2 = ConvBlock(64, 128, self.groups[1], name="conv2_")
+        self._conv_block_3 = ConvBlock(128, 256, self.groups[2], name="conv3_")
+        self._conv_block_4 = ConvBlock(256, 512, self.groups[3], name="conv4_")
+        self._conv_block_5 = ConvBlock(512, 512, self.groups[4], name="conv5_")
+
+        for idx, block in enumerate([
+                self._conv_block_1, self._conv_block_2, self._conv_block_3,
+                self._conv_block_4, self._conv_block_5
+        ]):
+            if self.stop_grad_layers >= idx + 1:
+                for param in block.parameters():
+                    param.trainable = False
+        if pretrained is not None:
+            load_pretrained_model(self, pretrained)
+
+    def forward(self, inputs):
+        feat_list = []
+        pool, x = self._conv_block_1(inputs)
+        feat_list.append(x)
+        pool, x = self._conv_block_2(pool)
+        feat_list.append(x)
+        pool, x = self._conv_block_3(pool)
+        feat_list.append(x)
+        pool, x = self._conv_block_4(pool)
+        feat_list.append(x)
+        pool, x = self._conv_block_5(pool)
+        feat_list.append(x)
+        return feat_list
+
+
+def VGG11(**args):
+    model = VGGNet(layers=11, **args)
+    return model
+
+
+def VGG13(**args):
+    model = VGGNet(layers=13, **args)
+    return model
+
+
+def VGG16(**args):
+    model = VGGNet(layers=16, **args)
+    return model
+
+
+def VGG19(**args):
+    model = VGGNet(layers=19, **args)
+    return model
+
