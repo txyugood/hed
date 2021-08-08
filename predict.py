57683cdd2f280ca1e5dbd5c7e72527500ef08a23
tongxingyu <tongxingyu@ln.chinamobile.com>
2021-08-08 08:43:25 +0800

    Format code.

--- predict.py
+++ predict.py
@@ -1,26 +1,14 @@
-import os
 import glob
+import os
+import argparse
 
 import cv2
-import scipy
 import numpy as np
 import paddle
 import paddle.nn.functional as F
 
-
 from hed_model import HED
 from utils import load_pretrained_model
-from transforms import Compose, Normalize
-from transforms import functional
-tiny = True
-
-if tiny:
-    target_path = 'output/data/tiny-edges'
-else:
-    target_path = 'output/result'
-
-if not os.path.exists(target_path):
-    os.makedirs(target_path)
 
 
 def tensor_flip(x, flip):
@@ -31,53 +19,77 @@ def tensor_flip(x, flip):
         x = x[:, :, ::-1, :]
     return x
 
-model = HED()
-load_pretrained_model(model, "/Users/alex/Downloads/model_hed.pdparams")
-model.eval()
-test_path = "/Users/alex/baidu/HED-BSDS/test/"
-if tiny:
-    image_list = [os.path.join(test_path, fn) for fn in ['2018.jpg', '3063.jpg', '5096.jpg', '14092.jpg', '35049.jpg',
-                                                         '6046.jpg', '41085.jpg', '28083.jpg', '101084.jpg',
-                                                         '246009.jpg']]
-else:
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Model training')
+    # params of testing
+
+    parser.add_argument(
+        '--pretrained_model',
+        dest='pretrained_model',
+        help='The directory for pretrained model',
+        type=str,
+        default='/Users/alex/Downloads/model_hed.pdparams')
+    parser.add_argument(
+        '--dataset',
+        dest='dataset',
+        help='The directory for test dataset',
+        type=str,
+        default='/Users/alex/baidu/HED-BSDS/test/')
+    parser.add_argument(
+        '--save_dir',
+        dest='save_dir',
+        help='The directory for saving the model snapshot',
+        type=str,
+        default='./output/result')
+
+    return parser.parse_args()
+
+
+def infer(args):
+    model = HED()
+    load_pretrained_model(model, args.pretrained_model)
+    model.eval()
+    target_path = args.save_dir
+    test_path = args.dataset
     image_list = glob.glob(test_path + "*.jpg")
-nimgs = len(image_list)
-print("totally {} images".format(nimgs))
-for i in range(nimgs):
-    img = image_list[i]
-    img = cv2.imread(img).astype(np.float32)
-    h, w, _ = img.shape
-    edge = np.zeros((h, w), np.float32)
-    scales = [0.5, 1, 1.5]
-    # flips = [[False, False], [True, False], [False, True],[True, True]]
+    nimgs = len(image_list)
+    print("totally {} images".format(nimgs))
+    for i in range(nimgs):
+        img = image_list[i]
+        img = cv2.imread(img).astype(np.float32)
+        h, w, _ = img.shape
+        edge = np.zeros((h, w), np.float32)
+        scales = [0.5, 1, 1.5]
+        flips = [[False, False]]
 
-    # scales = [1]
-    flips = [[False, False]]
-    # scales = [1]
+        for s in scales:
+            for flip in flips:
+                h1, w1 = int(s * h), int(s * w)
+                img1 = cv2.resize(img, (w1, h1), interpolation=cv2.INTER_CUBIC)
+                img1 = img1 - np.array((104.00699, 116.66876762, 122.67891434))
+                img1 = np.transpose(img1, [2, 0, 1])
+                img1 = img1[np.newaxis, :, :, :].astype(np.float32)
+                img1 = tensor_flip(img1, flip)
+                img1 = paddle.to_tensor(img1)
+                edge1 = model(img1)[0]
+                edge1 = F.sigmoid(edge1)
+                edge1 = tensor_flip(edge1, flip)
+                edge1 = edge1.numpy()[0, 0, :, :]
+                edge += cv2.resize(
+                    edge1, (w, h),
+                    interpolation=cv2.INTER_CUBIC).astype(np.float32)
+        edge /= (len(scales) * len(flips))
+        edge = edge / edge.max()
+        edge *= 255.0
+        fn, ext = os.path.splitext(image_list[i])
+        fn = fn.split('/')[-1]
 
-    for s in scales:
-        for flip in flips:
-            h1, w1 = int(s * h), int(s * w)
-            img1 = cv2.resize(img, (w1, h1), interpolation=cv2.INTER_CUBIC)
-            img1 = img1 - np.array((104.00699, 116.66876762, 122.67891434))
-            img1 = np.transpose(img1, [2, 0, 1])
-            img1 = img1[np.newaxis, :, :, :].astype(np.float32)
-            img1 = tensor_flip(img1, flip)
-            img1 = paddle.to_tensor(img1)
-            # img1 = F.interpolate(img1, (h1, w1), mode='bilinear').astype('float32')
-            edge1 = model(img1)[0]
-            # edge1 = F.interpolate(edge1, (h, w), mode='bilinear')
-            edge1 = F.sigmoid(edge1)
-            edge1 = tensor_flip(edge1, flip)
-            edge1 = edge1.numpy()[0, 0, :, :]
-            # edge += edge1.astype(np.float32)
-            # edge1 = edge1 / edge1.max()
-            edge += cv2.resize(edge1, (w, h), interpolation=cv2.INTER_CUBIC).astype(np.float32)
-    edge /= (len(scales) * len(flips))
-    edge = edge / edge.max()
-    edge *= 255.0
-    fn, ext = os.path.splitext(image_list[i])
-    fn = fn.split('/')[-1]
+        cv2.imwrite(os.path.join(target_path, "{}".format(fn) + '.png'), edge)
+        print("Saving to '" + os.path.join(target_path, image_list[i][0:-4]) +
+              "', Processing %d of %d..." % (i + 1, nimgs))
 
-    cv2.imwrite(os.path.join(target_path, "{}".format(fn) + '.png'), edge)
-    print("Saving to '" + os.path.join(target_path, image_list[i][0:-4]) + "', Processing %d of %d..." % (i + 1, nimgs))
+
+if __name__ == '__main__':
+    args = parse_args()
+    infer(args)
--- train.py
+++ train.py
@@ -1,65 +1,98 @@
 import os
 import time
+import argparse
 
 import paddle
-import paddle.nn.functional as F
-from dataset import Dataset
-from binary_cross_entropy_loss import BCELoss
-from hed_model import HED
-from transforms import Normalize, Resize, RandomDistort, RandomHorizontalFlip, RandomVerticalFlip, ResizeStepScaling,RandomPaddingCrop
-from timer import TimeAverager, calculate_eta
+
 import logger
+from binary_cross_entropy_loss import BCELoss
+from dataset import Dataset
+from hed_model import HED
+from timer import TimeAverager, calculate_eta
+from transforms import Normalize, Resize, RandomDistort, RandomHorizontalFlip, RandomVerticalFlip
 
-save_dir = 'output'
-iters = 100000
-save_interval = 1000
-batch_size = 10
-iter = 0
-log_iters = 100
-transforms = [
-    Resize(target_size=(400, 400)),
-    RandomHorizontalFlip(),
-    RandomVerticalFlip(),
-    RandomDistort(
-      brightness_range=0.4,
-      contrast_range=0.4,
-      saturation_range=0.4,
-    ),
-    # Normalize(mean=(104.00699, 116.66876762, 122.67891434), std=(57.375 ,57.12, 58.395))
-    Normalize(mean=(104.00699, 116.66876762, 122.67891434), std=(1, 1, 1))
-]
-dataset = Dataset(transforms=transforms,
-                  dataset_root="/home/aistudio/HED-BSDS",
-                  train_path="/home/aistudio/HED-BSDS/train_pair.lst")
 
-batch_sampler = paddle.io.DistributedBatchSampler(
-    dataset, batch_size=batch_size, shuffle=True, drop_last=True)
+def parse_args():
+    parser = argparse.ArgumentParser(description='Model training')
+    # params of training
+    parser.add_argument(
+        '--iters',
+        dest='iters',
+        help='iters for training',
+        type=int,
+        default=100000)
+    parser.add_argument(
+        '--batch_size',
+        dest='batch_size',
+        help='Mini batch size of one gpu or cpu',
+        type=int,
+        default=10)
+    parser.add_argument(
+        '--learning_rate',
+        dest='learning_rate',
+        help='Learning rate',
+        type=float,
+        default=1e-4)
+    parser.add_argument(
+        '--save_interval',
+        dest='save_interval',
+        help='How many iters to save a model snapshot once during training.',
+        type=int,
+        default=1000)
+    parser.add_argument(
+        '--pretrained_model',
+        dest='pretrained_model',
+        help='The directory for pretrained model',
+        type=str,
+        default='vgg.pdparams')
+    parser.add_argument(
+        '--dataset',
+        dest='dataset',
+        help='The directory for train dataset',
+        type=str,
+        default='vgg.pdparams')
+    parser.add_argument(
+        '--save_dir',
+        dest='save_dir',
+        help='The directory for saving the model snapshot',
+        type=str,
+        default='./output')
+    parser.add_argument(
+        '--log_iters',
+        dest='log_iters',
+        help='Display logging information at every log_iters',
+        default=10,
+        type=int)
 
-loader = paddle.io.DataLoader(
-    dataset,
-    batch_sampler=batch_sampler,
-    num_workers=0,
-    return_list=True,
-)
+    return parser.parse_args()
 
-iters_per_epoch = len(batch_sampler)
-avg_loss = 0.0
-avg_loss_list = []
-reader_cost_averager = TimeAverager()
-batch_cost_averager = TimeAverager()
-bce_loss = BCELoss(weight="dynamic")
-model = HED(backbone_pretrained='/home/aistudio/vgg16.pdparams')
 
-learning_rate = paddle.optimizer.lr.PolynomialDecay(learning_rate=1e-4, decay_steps=iters, power=0.9,end_lr=1e-8)
-lr = paddle.optimizer.lr.LinearWarmup(learning_rate=learning_rate, warmup_steps=10000, start_lr=0, end_lr=1e-4)
-optimizer = paddle.optimizer.Momentum(learning_rate=lr, parameters=model.parameters(), weight_decay=2e-4)
+def main(args):
+    save_dir = args.output
+    iters = args.iters
+    save_interval = args.save_interval
+    batch_size = args.batch_size
+    log_iters = args.log_iters
+    dataset_root = args.dataset
+    pretrained_model = args.pretrained_model
+    learning_rate = args.learning_rate
 
-batch_start = time.time()
-while iter < iters:
-
-    dataset = Dataset(transforms=transforms,
-                    dataset_root="/home/aistudio/HED-BSDS",
-                    train_path="/home/aistudio/HED-BSDS/train_pair.lst")
+    transforms = [
+        Resize(target_size=(400, 400)),
+        RandomHorizontalFlip(),
+        RandomVerticalFlip(),
+        RandomDistort(
+            brightness_range=0.4,
+            contrast_range=0.4,
+            saturation_range=0.4,
+        ),
+        # Normalize(mean=(104.00699, 116.66876762, 122.67891434), std=(57.375 ,57.12, 58.395))
+        Normalize(mean=(104.00699, 116.66876762, 122.67891434), std=(1, 1, 1))
+    ]
+    dataset = Dataset(
+        transforms=transforms,
+        dataset_root=dataset_root,
+        train_path=os.path.join(dataset_root, "train_pair.lst"))
 
     batch_sampler = paddle.io.DistributedBatchSampler(
         dataset, batch_size=batch_size, shuffle=True, drop_last=True)
@@ -71,68 +104,109 @@ while iter < iters:
         return_list=True,
     )
 
-    for data in loader:
-        iter += 1
-        if iter > iters:
-            break
-        images = data[0]
-        labels = data[1]
-        logits_list = model(images)
-        loss_list = []
-        for logits in logits_list:
-            loss = bce_loss(logits, labels)
-            loss_list.append(loss)
-        loss = sum(loss_list)
-        loss.backward()
-        optimizer.step()
-        model.clear_gradients()
+    iters_per_epoch = len(batch_sampler)
+    avg_loss = 0.0
+    avg_loss_list = []
+    reader_cost_averager = TimeAverager()
+    batch_cost_averager = TimeAverager()
+    bce_loss = BCELoss(weight="dynamic")
+    model = HED(backbone_pretrained=pretrained_model)
 
-        lr = optimizer.get_lr()
+    learning_rate = paddle.optimizer.lr.PolynomialDecay(
+        learning_rate=learning_rate, decay_steps=iters, power=0.9, end_lr=1e-8)
+    lr = paddle.optimizer.lr.LinearWarmup(
+        learning_rate=learning_rate,
+        warmup_steps=10000,
+        start_lr=0,
+        end_lr=1e-4)
+    optimizer = paddle.optimizer.Momentum(
+        learning_rate=lr, parameters=model.parameters(), weight_decay=2e-4)
 
-        # update lr
-        if isinstance(optimizer, paddle.distributed.fleet.Fleet):
-            lr_sche = optimizer.user_defined_optimizer._learning_rate
-        else:
-            lr_sche = optimizer._learning_rate
-        if isinstance(lr_sche, paddle.optimizer.lr.LRScheduler):
-            lr_sche.step()
+    iter = 0
+    batch_start = time.time()
+    while iter < iters:
 
-        avg_loss += loss.numpy()[0]
-        if not avg_loss_list:
-            avg_loss_list = [l.numpy() for l in loss_list]
-        else:
-            for i in range(len(loss_list)):
-                avg_loss_list[i] += loss_list[i].numpy()
-        batch_cost_averager.record(
-            time.time() - batch_start, num_samples=batch_size)
+        dataset = Dataset(
+            transforms=transforms,
+            dataset_root="/home/aistudio/HED-BSDS",
+            train_path="/home/aistudio/HED-BSDS/train_pair.lst")
 
-        if (iter) % log_iters == 0:
-            avg_loss /= log_iters
-            avg_loss_list = [l[0] / log_iters for l in avg_loss_list]
-            remain_iters = iters - iter
-            avg_train_batch_cost = batch_cost_averager.get_average()
-            avg_train_reader_cost = reader_cost_averager.get_average()
-            eta = calculate_eta(remain_iters, avg_train_batch_cost)
-            logger.info(
-                "[TRAIN] epoch: {}, iter: {}/{}, loss: {:.4f}, lr: {:.10f}, batch_cost: {:.4f}, reader_cost: {:.5f}, ips: {:.4f} samples/sec | ETA {}"
+        batch_sampler = paddle.io.DistributedBatchSampler(
+            dataset, batch_size=batch_size, shuffle=True, drop_last=True)
+
+        loader = paddle.io.DataLoader(
+            dataset,
+            batch_sampler=batch_sampler,
+            num_workers=0,
+            return_list=True,
+        )
+
+        for data in loader:
+            iter += 1
+            if iter > iters:
+                break
+            images = data[0]
+            labels = data[1]
+            logits_list = model(images)
+            loss_list = []
+            for logits in logits_list:
+                loss = bce_loss(logits, labels)
+                loss_list.append(loss)
+            loss = sum(loss_list)
+            loss.backward()
+            optimizer.step()
+            model.clear_gradients()
+
+            lr = optimizer.get_lr()
+
+            # update lr
+            if isinstance(optimizer, paddle.distributed.fleet.Fleet):
+                lr_sche = optimizer.user_defined_optimizer._learning_rate
+            else:
+                lr_sche = optimizer._learning_rate
+            if isinstance(lr_sche, paddle.optimizer.lr.LRScheduler):
+                lr_sche.step()
+
+            avg_loss += loss.numpy()[0]
+            if not avg_loss_list:
+                avg_loss_list = [l.numpy() for l in loss_list]
+            else:
+                for i in range(len(loss_list)):
+                    avg_loss_list[i] += loss_list[i].numpy()
+            batch_cost_averager.record(
+                time.time() - batch_start, num_samples=batch_size)
+
+            if (iter) % log_iters == 0:
+                avg_loss /= log_iters
+                avg_loss_list = [l[0] / log_iters for l in avg_loss_list]
+                remain_iters = iters - iter
+                avg_train_batch_cost = batch_cost_averager.get_average()
+                avg_train_reader_cost = reader_cost_averager.get_average()
+                eta = calculate_eta(remain_iters, avg_train_batch_cost)
+                logger.info(
+                    "[TRAIN] epoch: {}, iter: {}/{}, loss: {:.4f}, lr: {:.10f}, batch_cost: {:.4f}, reader_cost: {:.5f}, ips: {:.4f} samples/sec | ETA {}"
                     .format((iter - 1) // iters_per_epoch + 1, iter, iters,
                             avg_loss, lr, avg_train_batch_cost,
                             avg_train_reader_cost,
                             batch_cost_averager.get_ips_average(), eta))
 
-            avg_loss = 0.0
-            avg_loss_list = []
-            reader_cost_averager.reset()
-            batch_cost_averager.reset()
+                avg_loss = 0.0
+                avg_loss_list = []
+                reader_cost_averager.reset()
+                batch_cost_averager.reset()
 
-        if (iter % save_interval == 0 or iter == iters):
-            current_save_dir = os.path.join(save_dir,
-                                            "iter_{}".format(iter))
-            if not os.path.isdir(current_save_dir):
-                os.makedirs(current_save_dir)
-            paddle.save(model.state_dict(),
-                        os.path.join(current_save_dir, 'model.pdparams'))
-            paddle.save(optimizer.state_dict(),
-                        os.path.join(current_save_dir, 'model.pdopt'))
-        batch_start = time.time()
+            if (iter % save_interval == 0 or iter == iters):
+                current_save_dir = os.path.join(save_dir,
+                                                "iter_{}".format(iter))
+                if not os.path.isdir(current_save_dir):
+                    os.makedirs(current_save_dir)
+                paddle.save(model.state_dict(),
+                            os.path.join(current_save_dir, 'model.pdparams'))
+                paddle.save(optimizer.state_dict(),
+                            os.path.join(current_save_dir, 'model.pdopt'))
+            batch_start = time.time()
 
+
+if __name__ == '__main__':
+    args = parse_args()
+    main(args)
